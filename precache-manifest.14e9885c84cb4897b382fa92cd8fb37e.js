008e75f9b8d2b6bfbe7fd42ce1f40b0a58835311
Sanjay Patil <sp2004@hotmail.com>
2020-04-26 20:25:01 -0700

    Updates

---
+++ precache-manifest.14e9885c84cb4897b382fa92cd8fb37e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98935788b044e4e87b0572b97b393ce0",
+    "url": "/clickygame/index.html"
+  },
+  {
+    "revision": "502a3214d97d0e472738",
+    "url": "/clickygame/static/css/main.515b1c73.chunk.css"
+  },
+  {
+    "revision": "1bbf2b728c88ec37ec29",
+    "url": "/clickygame/static/js/2.bf395a0d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clickygame/static/js/2.bf395a0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "502a3214d97d0e472738",
+    "url": "/clickygame/static/js/main.b9cfa751.chunk.js"
+  },
+  {
+    "revision": "3f14f7be7edf70f2463f",
+    "url": "/clickygame/static/js/runtime-main.0e36e694.js"
+  },
+  {
+    "revision": "3e4901364de630bd58401137afb61096",
+    "url": "/clickygame/static/media/star-wars-lead.3e490136.jpg"
+  }
+]);
